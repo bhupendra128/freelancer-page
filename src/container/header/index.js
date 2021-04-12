78cb2cba1b66a6dd690ee694ffcdcd20c253fc2f
bhupendra kumar <bhupendra.kumar@publicissapient.com>
2021-04-12 18:37:21 +0530

    making header sticky

--- src/container/header/index.js
+++ src/container/header/index.js
@@ -1,13 +1,13 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import Button from "../../atom/button";
 import { HamburgerIcon, SearchIcon, FilterIcon } from "../../assets/image";
 import Modal from "../../atom/modal";
 import { filterData } from "../../assets/data";
 import FilterCards from "../../presentation/filter-cards";
 import { AppContext } from "../../context/AppContext";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
 import "./header.scss";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 /**
  * Header Component that will create header
@@ -15,40 +15,51 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Header = () => {
   const [isModalOpened, setIsModalOpened] = useState(false);
-  const { setListingData,initialData,} = useContext(AppContext);
+  const { setListingData, initialData } = useContext(AppContext);
 
   const handleFilterClick = () => {
     const bodyEle = window && window.document.body;
-    bodyEle.classList.add('no-scroll');
+    bodyEle.classList.add("no-scroll");
     setIsModalOpened(true);
   };
 
   const handleSearchClick = () => {
     toast("Coming Soon");
-  }
+  };
 
+  useEffect(() => {
+    const bodyEle = window.document.body;
+    bodyEle.classList.add("sticky");
+    return () => {
+      if (bodyEle.classList.contains("sticky")) {
+        bodyEle.classList.remove("sticky");
+      }
+    };
+  });
   const handleSelectedFilter = (arr) => {
-    let minimumRating = arr && arr.length ? arr[0].rating: '';
-    if(arr && arr.length > 1){
-      for(let val of arr){
-        if(val['rating'] < minimumRating){
-          minimumRating = val['rating'];
+    let minimumRating = arr && arr.length ? arr[0].rating : "";
+    if (arr && arr.length > 1) {
+      for (let val of arr) {
+        if (val["rating"] < minimumRating) {
+          minimumRating = val["rating"];
         }
       }
     }
-    const updatedListingData = initialData.filter(itm=>itm.avg_ratings >= minimumRating);
+    const updatedListingData = initialData.filter(
+      (itm) => itm.avg_ratings >= minimumRating
+    );
     setListingData(updatedListingData);
     const bodyEle = window && window.document.body;
-    if(bodyEle.classList.contains('no-scroll')){
-      bodyEle.classList.remove('no-scroll');
+    if (bodyEle.classList.contains("no-scroll")) {
+      bodyEle.classList.remove("no-scroll");
     }
-    
+
     setIsModalOpened(false);
   };
 
   return (
     <div className="c-header">
-    <ToastContainer />
+      <ToastContainer />
       <div className="header-content">
         <Button>
           <HamburgerIcon />
